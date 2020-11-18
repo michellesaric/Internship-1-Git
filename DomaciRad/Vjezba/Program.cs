bfed508981dd3a566bfdc0c00587da733a657d48
„michellesaric“ <msaric03@fesb.hr>
2020-11-18 22:22:06 +0100

    Nadogradnja starog koda

--- DomaciRad/Vjezba/Program.cs
+++ DomaciRad/Vjezba/Program.cs
@@ -10,12 +10,44 @@ namespace Vjezba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Upišite neki broj:");
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Upišite neki drugi broj:");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Uneseni brojevi su: " + a + " i " + b);
-            Console.WriteLine("Pull request");
+            int d;
+            string asti = "Da";
+            while (asti == "Da")
+            {
+                Console.WriteLine("Upišite neki broj:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Upišite neki drugi broj:");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Uneseni brojevi su: " + a + " i " + b);
+                Console.WriteLine("Unesite jedan znak za matematicku operaciju:");
+                char c = char.Parse(Console.ReadLine());
+                if (c == '+')
+                {
+                    d = a + b;
+                    Console.WriteLine("Rezultat je " + d);
+                }
+                else if (c == '-')
+                {
+                    d = a - b;
+                    Console.WriteLine("Rezultat je " + d);
+                }
+                else if (c == '*')
+                {
+                    d = a * b;
+                    Console.WriteLine("Rezultat je " + d);
+                }
+                else if (c == '/')
+                {
+                    if (b == '0')
+                        Console.WriteLine("Noup, nemoguća radnja, rezultat bi bio beskonacno.");
+                    double e = (double)a / b;
+                    Console.WriteLine("Rezultat je " + e);
+                }
+                else
+                    Console.WriteLine("Krivo upisan znak.");
+                Console.WriteLine("Zelite li nastaviti dalje? Upisite Da ili Ne");
+                asti = Console.ReadLine();
+            }
         }
     }
 }
