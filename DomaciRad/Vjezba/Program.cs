ca23821528108905fb419ea76d9967b8a27228fb
michellesaric <msaric03@fesb.hr>
2020-11-18 18:28:01 +0100

    Ovo je moj prvi commit za domaći rad

---
+++ DomaciRad/Vjezba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite neki broj:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Upišite neki drugi broj:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Uneseni brojevi su: " + a + " i " + b);
+        }
+    }
+}
